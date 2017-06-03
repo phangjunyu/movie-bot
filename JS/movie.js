16c820a21fe92744d210fb901ae29b7147158cf6
Joseph Ling <joseph.lingzb@gmail.com>
2017-06-03 09:02:37 +0800

    added joseph comments

--- JS/movie.js
+++ JS/movie.js
@@ -2,7 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var MovieSchema = new Schema ({
-
+	// JOSEPH: need to expand this. is timing best saved as a string or number?
+	// might be better to save timing as a number 0000 - 2359, then u can search for timings > the timing that the user gave
+	// also add in a location field. see the model for storeLocations in merchant model of vouch server
 	title: 	String,
 	cinema: String,
 	timing: String
--- JS/searchService.js
+++ JS/searchService.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose');
 var Movie = require('./movie');
 
 exports.searchMovieTitle = function(req, res, next){
+	//JOSEPH: don't use aggregate for normal db queries. just do Movie.find({title: req.title}, function(err, movie) { })
+	//P.S. does this even work? shouldnt it be req.body.title?
 	var query = {title : req.title};
 		Movie.aggregate([
 			{$match: query},
@@ -23,6 +25,12 @@ exports.findQuery = function(movie, showTime, callback){
     var query = {title: movie};
     var query1 = {timing: showTime};
 
+    // JOSEPH: u can do a Movie.find({title:movie, timing:showTime}, function(err, movieArray) {
+
+    //})
+
+    // dont use Caps in front of nonclasses, i.e. in this case movieArray is a variable so shouldnt be MovieArray
+
     Movie.find({$and: [  query, query1 ] } )
     .exec(function(err, MovieArray){
         if (err) return callback(err);
@@ -32,6 +40,8 @@ exports.findQuery = function(movie, showTime, callback){
     })}
 
 exports.searchMovieTiming = function(req, res, next){
+
+	//JOSEPH: again dont use aggregate when u can use find
 	var query = {timing : req.params.movie_timing};
 		Movie.aggregate([
 			{$match: query},
@@ -48,6 +58,7 @@ exports.searchMovieTiming = function(req, res, next){
 }
 
 exports.searchMovieLocation = function(req, res, next){
+	//JOSEPH: use find. there is no need for aggregate
 	var query = {timing : req.params.movie_timing};
 		Movie.aggregate([
 			{$match: query},
